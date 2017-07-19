49a33bef4c345c72a3bafd1ce184d7e8d1a8ed3a
Kun Ji <kji@southernwine.com>
2017-07-19 15:21:53 -0400

    initial commit

---
+++ src/main/resources/snippet/Snippet.java
@@ -0,0 +1,14 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		<html>
+		  <body>
+		    <b>Flow:</b> #[exception.event.flowConstruct] <br/>
+		    <b>Cause</b>: #[exception.getMessage()] <br/>
+		    <b>Payload:</b><br />#[payload]<br />
+		  </body>
+		</html>
+	}
+}
+
